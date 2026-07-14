a05628418f002f7a4c157300930cb6722decd92c
Jade <pangqihui52@hotmail.com>
2026-07-14 22:32:23 +0800

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Welcome from "./components/Welcome"
 import "./App.css"
 import About from "./components/About"
+import Contact from "./components/Contact"
 
 function App() {
   return (
     <>
       <Welcome />
       <About />
+      <Contact />
     </>
   )
 }
